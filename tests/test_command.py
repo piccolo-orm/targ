2327e1eb38f7b1d35dcf35b5f0bb2564424f6bb5
Daniel Townsend <dan@dantownsend.co.uk>
2021-01-25 20:32:36 +0000

    unit tests for boolean flags, help flags, and no command

--- tests/test_command.py
+++ tests/test_command.py
@@ -10,6 +10,13 @@ def add(a: int, b: int):
     print(a + b)
 
 
+def greeting(afternoon: bool = False):
+    if afternoon:
+        print("Good afternoon")
+    else:
+        print("Good morning")
+
+
 def print_(value: t.Any, *args, **kwargs):
     """
     When patching the builtin print statement, this is used as a side effect,
@@ -73,16 +80,86 @@ class CLITest(TestCase):
 
     def test_invalid_group_name(self):
         """
-        Make sure a command is run correctly, when the command has been
-        registered in a group.
+        Make sure invalid group names are rejected.
         """
         with self.assertRaises(ValueError):
             CLI().register(add, group_name="contains spaces")
 
-    def test_invalid_app_name(self):
+        # Shouldn't raise an exception
+        CLI().register(add, group_name="my_group")
+
+    def test_invalid_command_name(self):
         """
-        Make sure a command is run correctly, when the command has been
-        registered in a group.
+        Make sure invalid app names are rejected.
         """
         with self.assertRaises(ValueError):
             CLI().register(add, command_name="contains spaces")
+
+        # Shouldn't raise an exception
+        CLI().register(add, command_name="my_command")
+
+    @patch("targ.CLI.get_cleaned_args")
+    def test_no_command(self, get_cleaned_args):
+        """
+        If no command name is given, then help text should be shown.
+        """
+        get_cleaned_args.return_value = []
+        cli = CLI()
+        cli.register(add)
+
+        with patch("targ.CLI.get_help_text") as get_help_text:
+            cli.run()
+            get_help_text.assert_called_once()
+
+        # And just run it once without patching, to make sure no errors
+        # are raised
+        cli.run()
+
+    @patch("targ.CLI.get_cleaned_args")
+    def test_help_flag(self, get_cleaned_args):
+        """
+        If the --help flag is passed in, then help text should be shown.
+        """
+        get_cleaned_args.return_value = ["add", "--help"]
+        cli = CLI()
+        cli.register(add)
+
+        with patch("targ.Command.print_help") as print_help:
+            cli.run()
+            print_help.assert_called_once()
+
+        # And just run it once without patching, to make sure no errors
+        # are raised
+        cli.run()
+
+    @patch("targ.CLI.get_cleaned_args")
+    def test_bool_flag(self, get_cleaned_args):
+        """
+        Test the different formats for boolean flags.
+        """
+        cli = CLI()
+        cli.register(greeting)
+
+        with patch("builtins.print", side_effect=print_) as print_mock:
+            get_cleaned_args.return_value = ["greeting"]
+            cli.run()
+            print_mock.assert_called_with("Good morning")
+
+            for flag in (
+                "--afternoon=f",
+                "--afternoon=false",
+                "--afternoon=False",
+            ):
+                get_cleaned_args.return_value = ["greeting", flag]
+                cli.run()
+                print_mock.assert_called_with("Good morning")
+
+            for flag in (
+                "--afternoon",
+                "--afternoon=t",
+                "--afternoon=true",
+                "--afternoon=True",
+            ):
+                get_cleaned_args.return_value = ["greeting", flag]
+                cli.run()
+                print_mock.assert_called_with("Good afternoon")
