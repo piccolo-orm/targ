2f84fece356f25b4777375b0a8aab21f5676e7d1
Daniel Townsend <dan@dantownsend.co.uk>
2021-11-03 15:57:18 +0000

    watch targ folder for changes

--- docs/serve_docs.py
+++ docs/serve_docs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from livereload import Server, shell
 
-
 server = Server()
 server.watch("src/", shell("make html"))
+server.watch("../targ", shell("make html"))
 server.serve(root="_build/html")
